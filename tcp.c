6c90f65f02b5194bd675d972337ac5096f87978b
messenger2011 <tai.pham@vietgis.com.vn>
2017-02-23 19:26:37 +0700

    using zero-copy to relay data

--- tcp.c
+++ tcp.c
@@ -4,16 +4,19 @@
 #include <unistd.h>
 #include <netdb.h>
 //
-#define PACKET_SIZE 1024
+#define BUFF_SIZE 4096
+#define PROTOCOL "HTTP/1.0"
 //
 int init_sock(int port);
 int init_connection(char * host, int port);
 //
 void loop();
 void handle_client(int client_sock, struct sockaddr_in client);
-
+void send_headers(FILE * sockw, int status, char* title, char* extra_header, char* mime_type, int length);
+void send_error(FILE* sockw, int status, char* title, char* extra_header, char* text);
+void relay_data(char* method, char* path, char* protocol, FILE* remote_r, FILE * remote_w, FILE* client_r, FILE* client_w);
+//
 int proxy_fd;
-
 
 int init_sock(int port) {
     int s, opt = 1;
@@ -48,108 +51,115 @@ void loop() {
     }
 }
 
-int getheaderlength(int fd)
-{
-    int c = 0;
-
-    ssize_t e;
-    char tmp[10];
-    bzero((char*)tmp, 1);
-
-    do
-    {
-        e = recv(fd, tmp, 1, MSG_PEEK);
-        if (e > 0)
-        {
-            if (strcmp(tmp, "\n") == 0)
-                break;
-            else
-                c++;
-        }
-    }while (e > 0);
-
-    return c;
-}
-
 void handle_client(int c_fd, struct sockaddr_in client)
 {
     struct sockaddr_in request;
-    int flag = 0;
     char method[4], url[10000], protocol[10000], host[10000], path[10000];
     int i_port;
     long l_port;
 
-    //
+    char buffer[BUFF_SIZE];
 
-    char tmp[PACKET_SIZE];
-    bzero((char*)tmp, PACKET_SIZE);
-    ssize_t n;
-    if ((n = recv(c_fd, tmp, PACKET_SIZE, MSG_PEEK)) > 0)
+    FILE* sockr = fdopen(c_fd, "r");
+    FILE* sockw = fdopen(c_fd, "w");
+
+    int first_line;
+    long content_length;
+
+    if (fgets(buffer, sizeof(buffer), sockr) == (char*) 0)
     {
-        int idx = strchr(tmp, '\n') - tmp;
-        if (idx > 0)
+        send_error(sockw, 400, (char *) "Bad Request", (char *) '\0', (char *) "No Request Found");
+    }else{
+        if (sscanf(buffer, "%[^ ] %[^ ] %[^ ]", method, url, protocol) != 3)
         {
-            char buffer[idx];
-            bzero((char*)buffer, idx);
-            n = recv(c_fd, buffer, idx, 0);
-            if (n > 0)
+            send_error(sockw, 400, (char *) "Bad Request", (char *)  '\0', (char *) "Not Support Protocol");
+        }
+        if (url[0] == '\0'){
+            send_error(sockw, 400, (char *) "Bad Request", (char *)  '\0', (char *) "Null URL");
+        }
+        if (strncasecmp(url, "http://", 7) == 0)
+        {
+            if (sscanf(url, "http://%[^:/]:%d%s", host, &i_port, path) == 3)
+                l_port = (long) i_port;
+            else if (sscanf(url, "http://%[^:/]%s", host, path) == 2)
+                l_port = 80;
+            else if (sscanf(url, "http://%[^/]:%d", host, &i_port) == 2  || sscanf(url, "http://%[^/]", host) == 1)
             {
-                if (sscanf(buffer, "%[^ ] %[^ ] %[^ ]", method, url, protocol) != 3)
-                {
-                    send(c_fd,"400 : Bad Request\nNot Support Protocol", 1000, 0);
-                    exit(0);
-                }
-                if (url[0] == '\0'){
-                    send(c_fd, "400 : BAD REQUEST - Null URL.", 1000, 0);
-                    exit(0);
-                }
-                if (strncasecmp(url, "http://", 7) == 0)
-                {
-                    if (sscanf(url, "http://%[^:/]:%d%s", host, &i_port, path) == 3)
-                        l_port = (long) i_port;
-                    else if (sscanf(url, "http://%[^:/]%s", host, path) == 2)
-                        l_port = 80;
-                    else if (sscanf(url, "http://%[^/]:%d", host, &i_port) == 2  || sscanf(url, "http://%[^/]", host) == 1)
-                    {
-                        l_port = i_port ? 80 : (long) i_port;
-                        * path = '\0';
-                    }else
-                    {
-                        send(c_fd, "400 : BAD REQUEST - Cannot parse URL.", 1000, 0);
-                    }
-
-                    int sockfd = init_connection(host, l_port);
-                    if (sockfd > 0)
-                    {
-                        ssize_t n = send(sockfd, buffer, strlen(buffer), 0);
-                        if (n > 0)
-                        {
-                            do
-                            {
-                                bzero((char *)buffer, PACKET_SIZE);
-                                n = recv(sockfd, buffer, PACKET_SIZE, 0);
-                                if (n > 0)
-                                    send(c_fd, buffer, n, 0);
-                            }while(n>0);
-                        }else{
-                            send(c_fd, "404 - Cannot connect", 1000, 0);
-                        }
-                        close(sockfd);
-                    }else{
-                        send(c_fd, "400 : BAD REQUEST:", 1000, 0);
-                    }
-                    
-                }else{
-                    
-                }
+                l_port = i_port ? 80 : (long) i_port;
+                * path = '\0';
+            }else
+            {
+                send_error(sockw, 400, (char *) "Bad Request", (char *)  '\0', (char *) "Cannot parse URL.");
             }
+
+            int remote_fd = init_connection(host, l_port);
+
+            FILE* remote_r = fdopen(remote_fd, "r");
+            FILE* remote_w = fdopen(remote_fd, "w");
+
+            relay_data(method, path, protocol, remote_r, remote_w, sockr, sockw);
+        }else{
         }
     }
-
     close(c_fd);
 }
 
+void relay_data(char* method, char* path, char* protocol, FILE* remote_r, FILE * remote_w, FILE* client_r, FILE* client_w)
+{
+    char buffer[BUFF_SIZE], remote_protocol[100], s[100];
+    int first_line, status, ich;
+    long content_length, i;
 
+    fprintf(remote_w, "%s %s %s\r\n", method, path, protocol);
+    content_length = -1;
+    while (fgets(buffer, sizeof(buffer), client_r) != (char*) 0)
+    {
+        if (strcmp(buffer, "\n") == 0 || strcmp(buffer, "\r\n") == 0)
+            break;
+        fputs(buffer, remote_w);
+        if (strncasecmp(buffer, "Content-Length:", 15) == 0)
+            content_length = atol(&(buffer[15]));
+    }
+    fputs(buffer, remote_w);
+    fflush(remote_w);
+    if (content_length != -1) {
+        for (i = 0; i < content_length && (ich = getchar()) != EOF; ++i)
+            putc(ich, remote_w);
+    }
+    fflush(remote_w);
+    // relay data back to client
+    content_length = -1;
+    first_line = 1;
+    status = -1;
+    while (fgets(buffer, sizeof(buffer), remote_r) != (char*) 0)
+    {
+        if (strcmp(buffer, "\n") == 0 || strcmp(buffer, "\r\n") == 0)
+            break;
+        fputs(buffer, client_w);
+        if (first_line) {
+            (void) sscanf(buffer, "%[^ ] %d", remote_protocol, &status);
+            first_line = 0;
+        }
+        if (strncasecmp(buffer, "Content-Length:", 15) == 0)
+            content_length = atol(&(buffer[15]));
+    }
+    /* Add a response header. */
+    fputs("Connection: close\r\n", client_w);
+    fputs(buffer, client_w);
+    fflush(client_w);
+    /* Under certain circumstances we don't look for the contents, even
+    ** if there was a Content-Length.
+    */
+    if (strcasecmp(method, "HEAD") != 0 && status != 304)
+    {
+        /* Forward the content too, either counted or until EOF. */
+        for (i = 0; (content_length == -1 || i < content_length) && (ich = getc(remote_r)) != EOF; ++i)
+        {
+            putchar(ich);
+        }
+    }
+    fflush(stdout);
+}
 
 int init_connection(char * host, int port) {
     struct sockaddr_in server;
@@ -168,6 +178,42 @@ int init_connection(char * host, int port) {
         return -1;
     }
     return sfd;
+}
+
+void send_headers(FILE* sockw, int status, char* title, char* extra_header, char* mime_type, int length) {
+    fprintf(sockw, "%s %d %s\r\n", PROTOCOL, status, title);
+    fprintf(sockw, "Server: %s\r\n", "DEMO");
+    if (extra_header != (char*) 0)
+        fprintf(sockw, "%s\r\n", extra_header);
+    if (mime_type != (char*) 0)
+        fprintf(sockw, "Content-Type: %s\r\n", mime_type);
+    if (length >= 0)
+        fprintf(sockw, "Content-Length: %d\r\n", length);
+    fprintf(sockw, "Connection: close\r\n");
+    fprintf(sockw, "\r\n");
+    fflush(sockw);
+}
+
+void send_error(FILE* sockw, int status, char* title, char* extra_header, char* text) {
+    send_headers(sockw, status, title, extra_header, (char *) "text/html", -1);
+
+    fprintf(sockw, "\
+        <!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n\
+        <html>\n\
+          <head>\n\
+            <meta http-equiv=\"Content-type\" content=\"text/html;charset=UTF-8\">\n\
+            <title>%d %s</title>\n\
+          </head>\n\
+          <body bgcolor=\"#cc9999\" text=\"#000000\" link=\"#2020ff\" vlink=\"#4040cc\">\n\
+            <h4>%d %s</h4>\n\n",
+            status, title, status, title);
+    fprintf(sockw, "%s\n\n", text);
+    fprintf(sockw, "\
+            <hr>\n\
+            <address><a href=\"127.0.0.1\">Lightweight Proxy</a></address>\n\
+          </body>\n\
+        </html>\n");
+    fflush(sockw);
 }
 
 int main(int argc, char *argv[]) {
